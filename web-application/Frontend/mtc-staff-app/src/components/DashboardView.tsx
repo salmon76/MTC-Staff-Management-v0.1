ece6cc92fbb4b2d8f01bbab079d7e6755eeeebd2
SalmonBigball <apple@Salmons-MacBook-Air.local>
2026-02-23 14:07:34 +0700

    update equipment icon to camera icon

--- web-application/Frontend/mtc-staff-app/src/components/DashboardView.tsx
+++ web-application/Frontend/mtc-staff-app/src/components/DashboardView.tsx
@@ -284,7 +284,8 @@ export default function DashboardView({ userName }: DashboardProps) {
                             strokeLinecap="round"
                             strokeLinejoin="round"
                         >
-                            <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+                            <circle cx="12" cy="13" r="4" />
                         </svg>
                     }
                 />
